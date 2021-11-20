3150fb53e62d05d20d633d847051767d630c4ef0
Naim Abaz <94642009+Zzznai@users.noreply.github.com>
2021-11-20 11:17:13 +0300

    Update Program.cs

--- Homeworks/Document 13/Fortress/Program.cs
+++ Homeworks/Document 13/Fortress/Program.cs
@@ -7,9 +7,7 @@ namespace Fortress
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-
             int mid = n * 2 - 4 - 2 * (n / 2);
-
             Console.WriteLine("/{0}\\{1}/{2}\\", new string('^', n / 2), new string('_', mid), new string('^', n / 2));
             for (int i = 0; i < n - 3; i++)
             {
