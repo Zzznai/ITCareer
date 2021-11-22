0299e840e73834e249d557940ddb8171dcdf14bd
Naim Abaz <94642009+Zzznai@users.noreply.github.com>
2021-11-22 22:38:39 +0200

    Add files via upload

---
+++ Homeworks/Sample_test-1/3.Football game/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace footballgame.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sector=Console.ReadLine();
+            var rows=int.Parse(Console.ReadLine());
+            var seats=int.Parse(Console.ReadLine());
+            double profit=rows*seats;
+            if (sector == "Sector A")
+            {
+                profit = profit * 11.60;
+            }
+            else
+            if(sector =="Sector B")
+            {
+                profit = profit * 14.50;
+            }
+            else
+            if(sector=="Sector C")
+            {
+                profit = profit * 16.10;
+            }
+            else
+            {
+                profit = profit * 8.40;
+            }
+            Console.WriteLine("{0} profit is {1} lv.", sector, profit.ToString("F2"));
+
+
+        }
+    }
+}
