4daaeeeb802af6d2656a01a3e635784bb2f16b50
Naim Abaz <94642009+Zzznai@users.noreply.github.com>
2021-11-22 22:48:43 +0200

    Add files via upload

---
+++ Homeworks/Sample_test-1/5.Winning numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace winningnumbers.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            int winner = 0;
+            for(int i = 1; i <=9 ;i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    for (int k = 1; k <= 9; k++)
+                    {
+                        for (int l = 1; l <= 9; l++)
+                        {
+                            if (i + j == n && k % 2 == 0 && l == 3 || i + j == n && k % 2 == 0 && l == 6)
+                            {
+                                winner++;
+                                Console.Write("{0}{1}{2}{3} ",i,j,k,l);
+                            }
+
+                        }
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Count of winner numbers:{0}",winner);
+        }
+    }
+}
