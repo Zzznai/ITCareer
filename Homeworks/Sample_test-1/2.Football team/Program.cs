0df18bb8ae656b2460d5e148541e609918639eba
Naim Abaz <94642009+Zzznai@users.noreply.github.com>
2021-11-22 22:36:53 +0200

    Add files via upload

---
+++ Homeworks/Sample_test-1/2.Football team/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace footballteam.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tshirt=double.Parse(Console.ReadLine());
+            var shorts = tshirt - tshirt * 0.25;
+            var socks = shorts - shorts * 0.8;
+            var shoes = (tshirt + shorts) * 2;
+            var sum = (tshirt+shorts + socks + shoes)- (tshirt+shorts + socks + shoes) * 0.15;
+            var gift=double.Parse(Console.ReadLine());
+            
+            if(gift-sum>0)
+            {
+                Console.WriteLine("No, he will not earn the world-cup replica ball.");
+                Console.WriteLine("He needs {0} lv. more.", (gift-sum).ToString("F2"));
+            }
+            else
+            {
+                
+                Console.WriteLine("Yes, he will earn the world-cup replica ball!");
+                Console.WriteLine("His sum is {0} lv.",sum.ToString("F2"));
+
+            }
+
+        }
+    }
+}
