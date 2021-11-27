d1dbe6358d26947f1b8a3c0ac9df5dec77bcdf88
Naim Abaz <94642009+Zzznai@users.noreply.github.com>
2021-11-27 20:03:34 +0200

    Add files via upload

---
+++ Homeworks/Sample_test-2/1.Tiles_change/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace change_tiles.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = double.Parse(Console.ReadLine()); 
+            var w = double.Parse(Console.ReadLine()); 
+            var l = double.Parse(Console.ReadLine()); 
+            var m = double.Parse(Console.ReadLine()); 
+            var o = double.Parse(Console.ReadLine());
+            var area_playground = n * n;
+            var area_tile = w * l;
+            var area_bench = m * o;
+            var number_tiles = (area_playground - area_bench)/area_tile;
+            var time = number_tiles * 0.2;
+            Console.WriteLine(number_tiles);
+            Console.WriteLine(time);
+
+        }
+    }
+}
