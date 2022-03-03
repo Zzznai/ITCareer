d831d6c6ef4d1b9290a898f1f073b0f9d53196f0
Naim Abaz <94642009+Zzznai@users.noreply.github.com>
2022-03-03 19:04:46 +0300

    Update Program.cs

--- Homeworks/Homework-27.02/18_03/Program.cs
+++ Homeworks/Homework-27.02/18_03/Program.cs
@@ -1,1 +1,47 @@
+using System;
+using System.Linq;
 
+namespace _18._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var command = Console.ReadLine().Split(' ').ToList();
+            while(true)
+            {
+                var commands = command[0];
+                if (commands == "Even" || commands == "Odd")
+                {
+                    if (commands == "Even")
+                    {
+                        foreach (var num in nums)
+                        {
+                            if (num % 2 == 0)
+                                Console.Write(num + " ");
+                        }
+                    }
+                    else
+                    {
+                        foreach (var num in nums)
+                            if (num % 2 != 0)
+                                Console.Write(num + " ");
+                    }
+                    break;
+                }
+                var number = int.Parse(command[1]);
+                if (commands == "Insert")
+                {
+                    var index = int.Parse(command[2]);
+                    nums.Insert(index, number);
+                }
+                else
+                if (commands == "Delete")
+                    nums.RemoveAll(item => item == number);
+
+                command= Console.ReadLine().Split(' ').ToList();
+            }
+        }
+    }
+}
