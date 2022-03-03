9921771e5f22a4748908d56b629add0035ea4731
Naim Abaz <94642009+Zzznai@users.noreply.github.com>
2022-03-03 15:41:33 +0300

    Update Program.cs

--- Homeworks/Homework-27.02/17_02/Program.cs
+++ Homeworks/Homework-27.02/17_02/Program.cs
@@ -1,1 +1,15 @@
+using System;
+
+namespace _17._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums= Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            nums.Sort();
+            Console.WriteLine(string.Join(" <= ",nums));
+        }
+    }
+}
 
